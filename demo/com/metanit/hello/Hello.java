47a590ca45744420a3b8bb622d94bc9bcecc6de4
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-08-05 02:05:30 +0300

    Initial commit

---
+++ demo/com/metanit/hello/Hello.java
@@ -0,0 +1,12 @@
+// Зависимые модули
+
+package com.metanit.hello;
+
+import javax.swing.JOptionPane;
+
+public class Hello {
+    public static void main(String[] args){
+        //System.out.println("Hello Demo Module!");
+        JOptionPane.showMessageDialog(null, "Hello Demo Module!");
+    }
+}
